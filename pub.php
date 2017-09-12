bf7cd91b0f49abd089508fa4457d0fa0fa2bee6f
numpapickmaker <numpapickmaker@users.noreply.github.com>
2017-09-12 19:46:35 +0700

    Update pub.php

--- pub.php
+++ pub.php
@@ -45,7 +45,7 @@
       echo $response . "\r\n";
     return $response;
 }
- $Topic = "NodeMCU1";
- $lineMsg = "CHECK";
- getMqttfromlineMsg($Topic,$lineMsg);
+// $Topic = "NodeMCU1";
+ //$lineMsg = "CHECK";
+ //getMqttfromlineMsg($Topic,$lineMsg);
 ?>
