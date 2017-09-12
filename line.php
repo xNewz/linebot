b573ce99030b2037e59e705da9b0b480c815aeb7
numpapickmaker <numpapickmaker@users.noreply.github.com>
2017-09-12 20:04:01 +0700

    Update line.php

--- line.php
+++ line.php
@@ -13,7 +13,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'your-user-id',
+        'to' => 'Ue77a191627f6ac91899e75d92264310c',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
