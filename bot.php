33804cf758a7e1678bc7524d9f11ee6e0409a866
numpapickmaker <numpapickmaker@users.noreply.github.com>
2017-09-12 20:02:28 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,7 +10,7 @@ $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['ESP'])) {
 	
-	send_LINE($events['ESP'],$events['replyToken']);
+	send_LINE($events['ESP']);
 		
 	echo "OK";
 	}
